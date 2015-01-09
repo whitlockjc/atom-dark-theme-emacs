fa6b666d665e82a1c1395c61c6cad93bb879fd4c
Jeremy Whitlock <jwhitlock@apache.org>
2015-01-09 16:31:08 -0700

    Added autoload and minor documentation change

--- atom-dark-theme.el
+++ atom-dark-theme.el
@@ -96,4 +96,12 @@
  '(powerline-active2 ((t (:background "grey10"))))
  )
 
+;;;###autoload
+(and load-file-name
+     (boundp 'custom-theme-load-path)
+     (add-to-list 'custom-theme-load-path
+		  (file-name-as-directory (file-name-directory load-file-name))))
+
 (provide-theme 'atom-dark)
+
+;;; atom-dark-theme.el ends here
